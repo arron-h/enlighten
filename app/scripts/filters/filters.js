6236b47a805932b7f0409b6eb73b5e907f3ebeb9
Arron Hartley <arronhartley@gmail.com>
2014-12-22 21:57:28 +0000

    Added placeholder item counts. Added simple events service. Added a filter factory. Moved some query logic to the filter factory. Folders working pretty nicely.

---
+++ app/scripts/filters/filters.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name enlightenApp.factory:FilterFactory
+ * @description
+ * # FilterFactory
+ * Filters which can be used
+ */
+angular.module('enlightenApp')
+	.factory('FilterFactory', function()
+	{
+		var filters =
+		{
+			"Limit100": function()
+			{
+				this.executeQuery = function(db)
+				{
+					return db.exec("SELECT originalFilename FROM AgLibraryFile " +
+						"LIMIT 100");
+				}
+			},
+
+			"FolderFilter": function()
+			{
+				this.executeQuery = function(db)
+				{
+					var query;
+					var indices = -1;
+
+					if (this.folderIsRoot)
+					{
+						query = "SELECT id_local FROM AgLibraryFolder WHERE " +
+							"rootFolder=" + this.folderIndex.toString();
+
+						indices = "";
+						var folders = db.each(query,
+						function(row)
+						{
+							indices += row.id_local + ",";
+						},
+						function()
+						{
+							if (indices.length > 0)
+								indices = indices.slice(0, indices.length - 1);
+						});
+					}
+					else
+					{
+						indices = this.folderIndex;
+					}
+
+					query = "SELECT originalFilename FROM AgLibraryFile " +
+						"WHERE folder IN (" + indices + ") LIMIT 100";
+
+					return db.exec(query);
+				}
+
+				this.folderIndex  = 0;
+				this.folderIsRoot = false;
+			}
+		};
+
+		return {
+			defaultFilter: function()
+			{
+				return new filters["Limit100"];
+			},
+
+			getFilter: function(filterName)
+			{
+				return new filters[filterName];
+			}
+		}
+	});
